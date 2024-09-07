1faaeed22540101f88a8200e53a2cac5cb422199
Sagar Jadhav <sjsagar2000@gmail.com>
2024-09-07 19:05:25 +0530

    Solved miniSubarraySum in optimized way

--- Stack/miniSubarraySum.cpp
+++ Stack/miniSubarraySum.cpp
@@ -1,19 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
+ 
  int sumSubarrayMins(vector<int>& arr) {
-        int sum = 0;
-            int n = arr.size();
-            long long M = 1000000007;
-            for(int i=0;i<n;i++){
-                int mini = INT_MAX;
-                for(int j=i;j<n;j++){
-                    mini = min(mini,arr[j]);
-                    sum += mini;
-                }
-            }
-            return sum;
-        
+    int n = arr.size();
+    vector<int> left(n);
+    vector<int> right(n);
+    stack<int> st;
+
+    long long M = 1000000007;
+   
+    // for Previous Lesser Element PLE
+    for(int i=0;i<n;i++){
+        while(!st.empty() && arr[st.top()] > arr[i]){
+            st.pop();
+        }
+        left[i] = (st.empty()) ? i+1 : i - st.top();
+        st.push(i);
     }
+    // Next Smaller Element NSE
+
+    while (!st.empty()) st.pop();
+    
+
+    for(int i=n-1;i>=0;i--){
+        while(!st.empty() && arr[st.top()] >= arr[i]){
+            st.pop();
+        }
+        right[i] = (st.empty()) ? n-i : st.top() - i;
+        st.push(i);
+    } 
+    long long  result = 0;
+    
+    for(int i=0;i<n;i++){
+
+       long long contribution = (right[i] * left[i] * 1LL)%M;
+        contribution = (contribution*arr[i])%M;
+          result = (result + contribution)%M;
+
+    }
+        return result;
+
+    } 
 
 int main(){
     vector<int> arr{3,1,2,4};
